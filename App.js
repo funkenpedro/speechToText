689d90773abe8c909c08f2c7c9f88ab1187033c3
pedro <pedro@ncf.ca>
2023-11-21 19:32:40 -0500

    tested and working

--- App.js
+++ App.js
@@ -1,10 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, Button, View, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+import Voice from "@react-native-voice/voice";
+
+/*
+expo init expo-speech-to-text
+cd expo-speech-to-text
+expo install @react-native-voice/voice expo-dev-client
+
+Add the following to your app.json, inside the expo section:
+"plugins": [
+  [
+    "@react-native-voice/voice",
+    {
+      "microphonePermission": "Allow Voice to Text Tutorial to access the microphone",
+      "speechRecognitionPermission": "Allow Voice to Text Tutorial to securely recognize user speech"
+    }
+  ]
+]
+
+If you don't have eas installed then install using the following command:
+npm install -g eas-cli
+
+eas login
+eas build:configure
+
+Build for local development on iOS or Android:
+eas build -p ios --profile development --local
+OR
+eas build -p android --profile development --local
+
+May need to install the following to build locally (which allows debugging)
+npm install -g yarn
+brew install fastlane
+
+After building install on your device:
+For iOS (simulator): https://docs.expo.dev/build-reference/simulators/
+For Android: https://docs.expo.dev/build-reference/apk/
+
+Run on installed app:
+expo start --dev-client
+
+*/
 
 export default function App() {
+  let [started, setStarted] = useState(false);
+  let [results, setResults] = useState([]);
+
+  useEffect(() => {
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const startSpeechToText = async () => {
+    console.log(Voice.isAvailable());
+    await Voice.start("en-US");
+    setStarted(true);
+  };
+
+  const stopSpeechToText = async () => {
+    await Voice.stop();
+    setStarted(false);
+  };
+
+  const onSpeechResults = (result) => {
+    setResults(result.value);
+  };
+
+  const onSpeechError = (error) => {
+    console.log(error);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {!started ? (
+        <Button title="Start Speech to Text" onPress={startSpeechToText} />
+      ) : undefined}
+      {started ? (
+        <Button title="Stop Speech to Text" onPress={stopSpeechToText} />
+      ) : undefined}
+      {results.map((result, index) => (
+        <TouchableOpacity key={index}>
+          <Text>{result}</Text>
+        </TouchableOpacity>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +96,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
